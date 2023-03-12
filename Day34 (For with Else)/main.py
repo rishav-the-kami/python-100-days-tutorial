d19a4889b7e1ef4334bd07e74dc1b4e4c8fceec7
Rishav <105970051+rishav-the-kami@users.noreply.github.com>
2023-03-12 22:42:22 +0530

    Add files via upload

---
+++ Day34 (For with Else)/main.py
@@ -0,0 +1,45 @@
+# NOTE u can use Else with For and While Loops too
+for i in range(1, 6):
+    print(i)
+else:
+    print("Loop Over!")
+
+""" Output:
+1
+2
+3
+4
+5
+Loop Over!
+"""
+
+# so it means that after the loop has COMPLETED SUCCESSFULLY (COMPLETED SUCCESSFULLY is important), the else block will execute.
+
+
+
+# ur reaction: welp this ez. beri beri ez why tf do i read more
+# u stoopid af bish :skull: chup chap niche wale code ka output guess kar
+
+for i in range(1, 5):
+    print(i)
+    if i == 3:
+        break
+else:
+    print("Loop Over!")
+
+"""Output:
+1
+2
+3
+"""
+
+# it did not print Loop Over
+
+
+
+# NOTE so it means that the else block executes when the loop COMPLETES SUCCESSFULLY and not BREAK. (isi wajah se COMPLETED SUCCESSFULLY ko emphasise kiya itna)
+
+
+
+
+# NOTE same can be done with WHILE loop.
