99c9f4844cdfbc7ed29bdc6d0f0e69259bd98274
Rishav <105970051+rishav-the-kami@users.noreply.github.com>
2023-03-16 22:56:12 +0530

    Add files via upload

---
+++ Day39 (Short Hand if else)/main.py
@@ -0,0 +1,28 @@
+# FIRST OF ALL IT AINT THE TERNARY OPERATOR. SO SHUSH THE FUCK UP
+
+a = 500
+b = 500
+
+print("A") if a > b else print("=") if a == b else print("B")
+# Yup ur confused ik.
+# Do one thing.
+# Read this code in english. yes english.
+# so its like this
+# print A if a is more than b, else print = if a is equals to b, else print B
+# Now it easier for u?
+
+# NOTE what if i dont need any else statement. 
+# print("A") if a > b
+# just this much code will produce error. to avoid it we code like this
+
+print(69) if 69 > 21 else ""
+
+# yup u use a "" in the else..... dont ask me why idek.
+
+
+
+# :C alr now no sad :/ u can use it like ternary operator ;_;
+is_promit_gay = True
+
+promits_gf = "speepy" if is_promit_gay else "Bryonna"
+print(promits_gf)
